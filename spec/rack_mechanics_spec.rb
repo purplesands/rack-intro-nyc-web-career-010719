dc1194c4072e5f64b9af98c513939edf0884f65c
purplesands <mullenjoe13@gmail.com>
2019-01-28 10:06:44 -0500

    Done.

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper.rb'
 describe "Application" do
   it "should introduce itself" do
     get "/"
-    expect(last_response.body).to include ("Hello, my name is")
+    expect(last_response.body).to include ("Hello, my name is Joe.")
   end
 end
